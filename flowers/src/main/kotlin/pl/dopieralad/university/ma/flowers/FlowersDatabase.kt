500c0989c099cc73c86aac4bd76e1cee93504759
Daniel Dopierała <dopierala.daniel@gmail.com>
2019-09-04 17:46:32 +0200

    Implement "FlowersDatabase" as a singleton

--- flowers/src/main/kotlin/pl/dopieralad/university/ma/flowers/FlowersDatabase.kt
+++ flowers/src/main/kotlin/pl/dopieralad/university/ma/flowers/FlowersDatabase.kt
@@ -1,6 +1,8 @@
 package pl.dopieralad.university.ma.flowers
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import pl.dopieralad.university.ma.flowers.flower.Flower
 import pl.dopieralad.university.ma.flowers.flower.FlowerDao
@@ -9,4 +11,17 @@ import pl.dopieralad.university.ma.flowers.flower.FlowerDao
 abstract class FlowersDatabase : RoomDatabase() {
 
     abstract fun flowerDao(): FlowerDao
+
+    companion object {
+
+        private lateinit var INSTANCE: FlowersDatabase
+
+        fun getInstance(context: Context): FlowersDatabase {
+            if (::INSTANCE.isInitialized) {
+                INSTANCE = Room.databaseBuilder(context, FlowersDatabase::class.java, "flowers-database").build()
+            }
+
+            return INSTANCE
+        }
+    }
 }
